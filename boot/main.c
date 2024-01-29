1c6c878afb48858933b9b7096a3b6647d79402e8
hunsang <hoonsang99@korea.ac.kr>
2024-01-29 12:52:51 +0900

    10. make getchar with uart

--- boot/main.c
+++ boot/main.c
@@ -3,5 +3,10 @@
 
 void main(){
     putstr("Hello world\n");
+
+    for(uint32_t i=0; i<100; i++){
+        uint8_t ch = Hal_uart_get_char();
+        putchar(ch);
+    }
 }
 
--- hal/rvpb/Uart.c
+++ hal/rvpb/Uart.c
@@ -15,3 +15,19 @@ void Hal_uart_put_char(uint8_t ch){
     while (Uart->uartfr.bits.TXFF);
     Uart->uartdr.all = (ch & 0xff);
 }
+
+uint8_t Hal_uart_get_char(void){
+    uint32_t data;
+
+    //RX buffer에 값이 들어올때 까지 대기
+    while(Uart->uartfr.bits.RXFE);
+
+    data = Uart->uartdr.all;
+
+    if(data & 0xf00){
+        Uart->uartrsr.all = 0xf;
+        return 0;
+    }
+
+    return (uint8_t)(data & 0xff);
+}
