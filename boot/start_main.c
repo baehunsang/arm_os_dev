6eba9bac55f2e68b5e6c406a72861e7e14ab68c0
hunsang <hoonsang99@korea.ac.kr>
2024-01-30 19:37:57 +0900

    make printf_test as function

--- boot/start_main.c
+++ boot/start_main.c
@@ -7,9 +7,19 @@ static void Hw_init(){
     Hal_uart_init();
 }
 
-void banner(){
+void printf_test(){
     char* str = "printf_test_str_in_stack";
     uint32_t i = 5;
+    debug_printf("################# printf test #####################\n");
+    debug_printf("%s\n", "hello printf");
+    debug_printf("%s\n", str);
+    debug_printf("%s\n", 0);
+    debug_printf("%u == 5 \n", i);
+    debug_printf("%x == 0xff, %u == 255\n", 0xff, 0xff);
+    debug_printf("%u\n", 0);
+}
+
+void banner(){
     putchar('\n');
     for(uint32_t i=0; i< 100; i++){
         putchar('#');
@@ -19,20 +29,12 @@ void banner(){
         putchar('#');
     }
     putstr("\n\n");
-
-    debug_printf("################# printf test #####################\n");
-    debug_printf("%s\n", "hello printf");
-    debug_printf("%s\n", str);
-    debug_printf("%s\n", 0);
-    debug_printf("%u == 5 \n", i);
-    debug_printf("%x == 0xff, %u == 255\n", 0xff, 0xff);
-    debug_printf("%u\n", 0);
-
-
 }
+
 
 void start_main(){
     Hw_init();
     banner();
+    //printf_test();
     main();
 }
