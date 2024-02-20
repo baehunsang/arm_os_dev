76655957bc4bfb7283e8af777e36a8ebcbfcfa1b
hunsang <hoonsang99@korea.ac.kr>
2024-02-20 13:23:24 +0900

    14. make timer

--- boot/start_main.c
+++ boot/start_main.c
@@ -1,15 +1,20 @@
 #include "stdint.h"
 #include "stdio.h"
 #include "HalInterrupt.h"
+#include "stdlib.h"
+#include "stdbool.h"
+
 
 static void Hw_init(){
     Hal_interrupt_init();
     Hal_uart_init();
+    Hal_timer_init();
 }
 
 void printf_test(){
     char* str = "printf_test_str_in_stack";
     uint32_t i = 5;
+    uint32_t* sysctrl0 = (uint32_t*)0x10001000;
     debug_printf("################# printf test #####################\n");
     debug_printf("%s\n", "hello printf");
     debug_printf("%s\n", str);
@@ -17,6 +22,7 @@ void printf_test(){
     debug_printf("%u == 5 \n", i);
     debug_printf("%x == 0xff, %u == 255\n", 0xff, 0xff);
     debug_printf("%u\n", 0);
+    debug_printf("SYSCTRL0 %x\n", *sysctrl0);
 }
 
 void banner(){
@@ -31,10 +37,18 @@ void banner(){
     putstr("\n\n");
 }
 
+static void Timer_test(){
+    while(true){
+        debug_printf("current count: %u\n", Hal_timer_get_1ms_counter());
+        delay(1000);
+    }
+}
+
 
 void start_main(){
     Hw_init();
     banner();
     //printf_test();
-    main();
+    Timer_test();
+    //main();
 }
---
+++ hal/HalTimer.h
@@ -0,0 +1,8 @@
+#ifndef HAL_HALTIMER_H
+#define HAL_HALTIMER_H
+
+void Hal_timer_init();
+
+uint32_t Hal_timer_get_1ms_counter(void);
+
+#endif
